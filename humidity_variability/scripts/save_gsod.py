cb1e1ed619d3d9d12f39c42830a5fd928bb8dfb3
karenamckinnon <kmckinnon@g.ucla.edu>
2019-07-15 21:27:25 +0000

    updated to get full records

--- humidity_variability/scripts/save_gsod.py
+++ humidity_variability/scripts/save_gsod.py
@@ -5,18 +5,19 @@ TODO: add command line args
 """
 
 import os
-from helpful_utilities import download_gsod, meteo
+from helpful_utilities import download_gsod
 import ctypes
 
 
 start_year = 1973
 end_year = 2018
+expand_data = True  # should the start/end year be the edges of the data, or a minimum requirement?
 search_query = {'begin': 'datetime(%i, 1, 1)' % start_year,
                 'end': 'datetime(%i, 12, 31)' % end_year}
 
 n_tries = 5  # Number of times to try (1) download and (2) save
 
-hashable = tuple((tuple(search_query.keys()), tuple(search_query.values())))
+hashable = tuple((tuple(search_query.keys()), tuple(search_query.values()), expand_data))
 query_hash = str(ctypes.c_size_t(hash(hashable)).value)  # ensures positive value
 savedir = '/home/mckinnon/bucket/gsod/'
 
@@ -34,9 +35,14 @@ meta_savename = '%s%s/metadata.csv' % (savedir, query_hash)
 if not os.path.isfile(meta_savename):
     metadata.to_csv(meta_savename)
 
+# Create new metadata dataframe with correct start and end dates
+new_meta = metadata.copy()
+
 for row in metadata.iterrows():
 
     station = row[1]['station_id']
+    this_start_year = row[1]['begin'].year
+    this_end_year = row[1]['end'].year
 
     data_savename = '%s%s/%s.csv' % (savedir, query_hash, station)
     if not os.path.isfile(data_savename):
@@ -46,8 +52,8 @@ for row in metadata.iterrows():
         while this_try < n_tries:
             try:
                 df = download_gsod.get_data(station=station,
-                                            start=start_year,
-                                            end=end_year)
+                                            start=this_start_year,
+                                            end=this_end_year)
                 break
             except Exception as e:
                 print('Try %i to download, error: %s' % (this_try, e.args))
@@ -58,16 +64,14 @@ for row in metadata.iterrows():
         if df.empty:
             continue
 
-        # Convert to Celsius
-        df['temp'] = meteo.F_to_C(df['temp'])
-        df['dewp'] = meteo.F_to_C(df['dewp'])
-        df['max_temp'] = meteo.F_to_C(df['max_temp'])
-        df['min_temp'] = meteo.F_to_C(df['min_temp'])
-
         # Drop columns we're not going to use
         df = df.drop(['visib', 'visib_c', 'gust', 'f', 'r', 's', 'h', 'th', 'tr'], axis=1)
 
-        # Allow for re-tries
+        # Update metadata file with true start/end
+        new_meta.loc[new_meta['station_id'] == station, 'begin'] = df['date'][0]
+        new_meta.loc[new_meta['station_id'] == station, 'end'] = df['date'][len(df)-1]
+
+        # Save data to csv
         this_try = 0
         while this_try < n_tries:
             try:
@@ -78,3 +82,5 @@ for row in metadata.iterrows():
                 this_try += 1
         else:
             continue
+
+new_meta.to_csv('%s%s/new_metadata.csv' % (savedir, query_hash))
