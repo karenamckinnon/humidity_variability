b9440f6de4b6e65b6014fab032d57244b6087e23
karenamckinnon <kmckinnon@g.ucla.edu>
2019-08-07 19:49:09 +0000

    changed fit to use custom code

--- humidity_variability/tools/model_fits.py
+++ humidity_variability/tools/model_fits.py
@@ -1,9 +1,11 @@
-import statsmodels.formula.api as smf
+# import statsmodels.formula.api as smf
 from humidity_variability.examples.single_bayesian_QR import log_AL
 import numpy as np
+from humidity_variability.utils import solve_qr
+import patsy
 
 
-def fit_zero_knots(df_use, this_q):
+def fit_zero_knots(df_use, this_q, constraint, q=None):
     """Fit QR model with zero knots.
 
     Parameters
@@ -12,6 +14,13 @@ def fit_zero_knots(df_use, this_q):
         Contains data to be fit (columns = temp_j, dewp_j, GMT)
     this_q : float
         Quantile to fit
+    constraint : str
+        Type of constraint to impose: 'None', 'Below', 'Above'.
+        'Below' indicates no crossing of lower quantile (i.e. tau = 0.55 shouldn't cross tau = 0.5)
+        'Above' indicates no crossing of upper quantile (i.e. tau = 0.45 shouldn't cross tau = 0.5)
+        'None' imposes no constraints, and should be used for estimating the median quantile.
+    q : numpy.ndarray or None
+        The fitted quantile not to be crossed (if constraint is not None)
 
     Returns
     -------
@@ -25,19 +34,24 @@ def fit_zero_knots(df_use, this_q):
     """
     # Zero knots
     f = 'dewp_j ~ GMT*bs(temp_j, degree=1, df=1)'
-    mod = smf.quantreg(f, df_use)
-    res = mod.fit(q=this_q, max_iter=10000)
-    yhat = res.fittedvalues
+    # mod = smf.quantreg(f, df_use)
+    # res = mod.fit(q=this_q, max_iter=10000)
+    _, X = patsy.dmatrices(f, df_use, return_type='matrix')
+    beta, yhat = solve_qr(X, df_use['dewp_j'].values, this_q, constraint, q)
+
+    # yhat = res.fittedvalues
     loglike = log_AL(df_use['dewp_j'].values, yhat, 1, this_q)
-    n = res.nobs
-    k = len(res.params)
+    n = len(df_use)
+    k = X.shape[1]
+    # n = res.nobs
+    # k = len(res.params)
 
     BIC = np.log(n)*k - 2*loglike
 
-    return BIC, res, f
+    return BIC, beta, f
 
 
-def fit_one_knot(df_use, N, this_q, BIC_prior):
+def fit_one_knot(df_use, N, this_q, BIC_prior, constraint, q=None):
     """Fit QR model with a single knot.
 
     Parameters
@@ -51,6 +65,13 @@ def fit_one_knot(df_use, N, this_q, BIC_prior):
         Quantile to fit
     BIC_prior : float
         The BIC from the simpler model
+    constraint : str
+        Type of constraint to impose: 'None', 'Below', 'Above'.
+        'Below' indicates no crossing of lower quantile (i.e. tau = 0.55 shouldn't cross tau = 0.5)
+        'Above' indicates no crossing of upper quantile (i.e. tau = 0.45 shouldn't cross tau = 0.5)
+        'None' imposes no constraints, and should be used for estimating the median quantile.
+    q : numpy.ndarray or None
+        The fitted quantile not to be crossed (if constraint is not None)
 
     Returns
     -------
@@ -71,9 +92,11 @@ def fit_one_knot(df_use, N, this_q, BIC_prior):
 
     for kk in range(N):
         f = 'dewp_j ~ GMT*bs(temp_j, degree=1, df=2, knots=np.array([%0.2f]))' % proposed_knots[kk]
-        mod = smf.quantreg(f, df_use)
-        res = mod.fit(q=this_q, max_iter=10000)
-        yhat = res.fittedvalues
+        # mod = smf.quantreg(f, df_use)
+        # res = mod.fit(q=this_q, max_iter=10000)
+        # yhat = res.fittedvalues
+        _, X = patsy.dmatrices(f, df_use, return_type='matrix')
+        beta, yhat = solve_qr(X, df_use['dewp_j'].values, this_q, constraint, q)
         loglike[kk] = log_AL(df_use['dewp_j'].values, yhat, 1, this_q)
 
     new_range = np.sort(proposed_knots[np.argsort(loglike)[::-1][:2]])
@@ -81,27 +104,33 @@ def fit_one_knot(df_use, N, this_q, BIC_prior):
 
     for kk in range(N):
         f = 'dewp_j ~ GMT*bs(temp_j, degree=1, df=2, knots=np.array([%0.2f]))' % proposed_knots[kk]
-        mod = smf.quantreg(f, df_use)
-        res = mod.fit(q=this_q, max_iter=10000)
-        yhat = res.fittedvalues
+        # mod = smf.quantreg(f, df_use)
+        # res = mod.fit(q=this_q, max_iter=10000)
+        # yhat = res.fittedvalues
+        _, X = patsy.dmatrices(f, df_use, return_type='matrix')
+        beta, yhat = solve_qr(X, df_use['dewp_j'].values, this_q, constraint, q)
         loglike[kk] = log_AL(df_use['dewp_j'].values, yhat, 1, this_q)
 
     best_knot = proposed_knots[np.argmax(loglike)]
     f = 'dewp_j ~ GMT*bs(temp_j, degree=1, df=2, knots=np.array([%0.2f]))' % best_knot
-    mod = smf.quantreg(f, df_use)
-    res = mod.fit(q=this_q, max_iter=10000)
-    yhat = res.fittedvalues
+    # mod = smf.quantreg(f, df_use)
+    # res = mod.fit(q=this_q, max_iter=10000)
+    # yhat = res.fittedvalues
+    _, X = patsy.dmatrices(f, df_use, return_type='matrix')
+    beta, yhat = solve_qr(X, df_use['dewp_j'].values, this_q, constraint, q)
     loglike = log_AL(df_use['dewp_j'].values, yhat, 1, this_q)
-    n = res.nobs
-    k = len(res.params)
+    n = len(df_use)
+    k = X.shape[1]
+    # n = res.nobs
+    # k = len(res.params)
 
     BIC = np.log(n)*k - 2*loglike
     delta_bic = BIC - BIC_prior
 
-    return BIC, delta_bic, res, f
+    return BIC, delta_bic, beta, f
 
 
-def fit_two_knots(df_use, N, this_q, BIC_prior):
+def fit_two_knots(df_use, N, this_q, BIC_prior, constraint, q=None):
     """Fit QR model with a single knot.
 
     Parameters
@@ -115,6 +144,13 @@ def fit_two_knots(df_use, N, this_q, BIC_prior):
         Quantile to fit
     BIC_prior : float
         The BIC from the simpler model
+    constraint : str
+        Type of constraint to impose: 'None', 'Below', 'Above'.
+        'Below' indicates no crossing of lower quantile (i.e. tau = 0.55 shouldn't cross tau = 0.5)
+        'Above' indicates no crossing of upper quantile (i.e. tau = 0.45 shouldn't cross tau = 0.5)
+        'None' imposes no constraints, and should be used for estimating the median quantile.
+    q : numpy.ndarray or None
+        The fitted quantile not to be crossed (if constraint is not None)
 
     Returns
     -------
@@ -139,29 +175,35 @@ def fit_two_knots(df_use, N, this_q, BIC_prior):
     for kk in range(N):
         f = 'dewp_j ~ GMT*bs(temp_j, degree=1, df=3, knots=np.array([%0.2f, %0.2f]))' % (proposed_knots[0, kk],
                                                                                          proposed_knots[1, kk])
-        mod = smf.quantreg(f, df_use)
-        res = mod.fit(q=this_q, max_iter=10000)
-        yhat = res.fittedvalues
+        # mod = smf.quantreg(f, df_use)
+        # res = mod.fit(q=this_q, max_iter=10000)
+        # yhat = res.fittedvalues
+        _, X = patsy.dmatrices(f, df_use, return_type='matrix')
+        beta, yhat = solve_qr(X, df_use['dewp_j'].values, this_q, constraint, q)
         loglike[kk] = log_AL(df_use['dewp_j'].values, yhat, 1, this_q)
 
     best_knot = proposed_knots[:, np.argmax(loglike)]
     f = 'dewp_j ~ GMT*bs(temp_j, degree=1, df=3, knots=np.array([%0.2f, %0.2f]))' % (best_knot[0],
                                                                                      best_knot[1])
-    mod = smf.quantreg(f, df_use)
-    res = mod.fit(q=this_q, max_iter=10000)
-    yhat = res.fittedvalues
+    # mod = smf.quantreg(f, df_use)
+    # res = mod.fit(q=this_q, max_iter=10000)
+    # yhat = res.fittedvalues
+    _, X = patsy.dmatrices(f, df_use, return_type='matrix')
+    beta, yhat = solve_qr(X, df_use['dewp_j'].values, this_q, constraint, q)
     loglike = log_AL(df_use['dewp_j'].values, yhat, 1, this_q)
-    n = res.nobs
-    k = len(res.params)
+    n = len(df_use)
+    k = X.shape[1]
+    # n = res.nobs
+    # k = len(res.params)
 
     BIC = np.log(n)*k - 2*loglike
 
     delta_bic = BIC - BIC_prior
 
-    return BIC, delta_bic, res, f
+    return BIC, delta_bic, beta, f
 
 
-def fit_three_knots(df_use, N, this_q, BIC_prior):
+def fit_three_knots(df_use, N, this_q, BIC_prior, constraint, q=None):
     """Fit QR model with a single knot.
 
     Parameters
@@ -175,6 +217,13 @@ def fit_three_knots(df_use, N, this_q, BIC_prior):
         Quantile to fit
     BIC_prior : float
         The BIC from the simpler model
+    constraint : str
+        Type of constraint to impose: 'None', 'Below', 'Above'.
+        'Below' indicates no crossing of lower quantile (i.e. tau = 0.55 shouldn't cross tau = 0.5)
+        'Above' indicates no crossing of upper quantile (i.e. tau = 0.45 shouldn't cross tau = 0.5)
+        'None' imposes no constraints, and should be used for estimating the median quantile.
+    q : numpy.ndarray or None
+        The fitted quantile not to be crossed (if constraint is not None)
 
     Returns
     -------
@@ -201,22 +250,28 @@ def fit_three_knots(df_use, N, this_q, BIC_prior):
         f = 'dewp_j ~ GMT*bs(temp_j, degree=1, df=4, knots=np.array([%0.2f, %0.2f, %0.2f]))' % (proposed_knots[0, kk],
                                                                                                 proposed_knots[1, kk],
                                                                                                 proposed_knots[2, kk])
-        mod = smf.quantreg(f, df_use)
-        res = mod.fit(q=this_q, max_iter=10000)
-        yhat = res.fittedvalues
+        # mod = smf.quantreg(f, df_use)
+        # res = mod.fit(q=this_q, max_iter=10000)
+        # yhat = res.fittedvalues
+        _, X = patsy.dmatrices(f, df_use, return_type='matrix')
+        beta, yhat = solve_qr(X, df_use['dewp_j'].values, this_q, constraint, q)
         loglike[kk] = log_AL(df_use['dewp_j'].values, yhat, 1, this_q)
 
     best_knot = proposed_knots[:, np.argmax(loglike)]
     f = 'dewp_j ~ GMT*bs(temp_j, degree=1, df=3, knots=np.array([%0.2f, %0.2f]))' % (best_knot[0],
                                                                                      best_knot[1])
-    mod = smf.quantreg(f, df_use)
-    res = mod.fit(q=this_q, max_iter=10000)
-    yhat = res.fittedvalues
+    # mod = smf.quantreg(f, df_use)
+    # res = mod.fit(q=this_q, max_iter=10000)
+    # yhat = res.fittedvalues
+    _, X = patsy.dmatrices(f, df_use, return_type='matrix')
+    beta, yhat = solve_qr(X, df_use['dewp_j'].values, this_q, constraint, q)
     loglike = log_AL(df_use['dewp_j'].values, yhat, 1, this_q)
-    n = res.nobs
-    k = len(res.params)
+    n = len(df_use)
+    k = X.shape[1]
+    # n = res.nobs
+    # k = len(res.params)
 
     BIC = np.log(n)*k - 2*loglike
     delta_bic = BIC - BIC_prior
 
-    return BIC, delta_bic, res, f
+    return BIC, delta_bic, beta, f
