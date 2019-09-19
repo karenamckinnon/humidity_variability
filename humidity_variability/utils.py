450d5f854c8a3c6b6c28bba68d65a43e2891f709
karenamckinnon <kmckinnon@g.ucla.edu>
2019-09-19 00:01:33 +0000

    tried additional reg for high dim

--- humidity_variability/utils.py
+++ humidity_variability/utils.py
@@ -440,6 +440,7 @@ def calc_SIC(beta, yhat, data, tau, delta, G, thresh=1e-4):
     u = data - yhat
     rho = u*(tau - (u < 0).astype(float))
 
-    SIC = np.log(np.sum(rho)) + np.log(N)/(2*N)*p_lambda
+    # https://www.tandfonline.com/doi/pdf/10.1080/01621459.2013.836975?needAccess=true
+    SIC = np.log(np.sum(rho)) + np.log(N)/(2*N)*p_lambda*np.log(len(beta))
 
     return SIC
